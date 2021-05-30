d4627468ff8e4ebbc2b509b8ed2192814d90dedb
oxalcvasquez <oxalc0101@gmail.com>
2021-05-30 17:15:41 -0500

    Agenda v3 mejorada

--- Agenda/js/app.js
+++ Agenda/js/app.js
@@ -24,23 +24,63 @@ function saveContacto(e) {
   }
   getContacto();
 }
+function recuperarContacto() {
+  console.log("hol");
+  const contactos = JSON.parse(localStorage.getItem("contactos"));
+  const nombre = document.getElementById("nombre").value;
+  localStorage.getItem(nombre);
+  document.getElementById("movil").value = localStorage.getItem(nombre);
+  for (let i=0;i<contactos.length;i++){
+  if (contactos[i].nombre == nombre) {
+    document.getElementById("movil").value = contactos[i].movil;
+    document.getElementById("email").value = contactos[i].email;
+  }
+}
+}
+
+function eliminarContacto(nombre){
+  const contactos = JSON.parse(localStorage.getItem("contactos"));
+  for (let i=0;i<contactos.length;i++){
+    if (contactos[i].nombre == nombre) {
+      contactos.splice(i, 1); // splice quitar un dato del arreglo el indice y el numero
+    }
+  }
+  localStorage.setItem("contactos", JSON.stringify(contactos));
+  getContacto();
+}
+
+function recuperarDatos() {
+  const nombre = document.getElementById("nombre").value;
+  localStorage.getItem(nombre);
+  document.getElementById("movil").value = localStorage.getItem(nombre);
+  document.getElementById("email").value = localStorage.getItem(nombre);
+}
+
+
+function eliminarTodo() {
+  localStorage.clear();
+  getContacto();
+}
 
 function getContacto() {
   const contactos = JSON.parse(localStorage.getItem("contactos"));
   const contactosView = document.getElementById('contactos');
   contactosView.innerHTML = ' '; // limpio en caso exista nuevo datos
-  for (let i = 0; i < contactos.length; i++) {
-    const nombre = contactos[i].nombre;
-    const movil = contactos[i].movil;
-    const email = contactos[i].email;
-    contactosView.innerHTML += `
-    <div class="contacto-card">
-      <div class="card-body">
-        <p>${nombre} - ${movil} - ${email} </p>
-      </div>
-    </div>`;
-
+  if(contactos!=null){
+     for (let i = 0; i < contactos.length; i++) {
+       const nombre = contactos[i].nombre;
+       const movil = contactos[i].movil;
+       const email = contactos[i].email;
+       contactosView.innerHTML += `
+      <div class="contacto-card">
+        <div class="card-body">
+          <p>${nombre} - ${movil} - ${email} </p>
+          <a class="btnEliminar" onclick="eliminarContacto('${nombre}')">Eliminar </a>
+        </div>
+      </div>`;
+     }
   }
+
 
 }
 getContacto();
